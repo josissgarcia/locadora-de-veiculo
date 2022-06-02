b0c24b4b87ee47ede3b57a1692fe7e0f8d915584
den-duar <2021211510079@iesp.edu.br>
2022-06-02 01:17:45 -0300

    Adicionado serialização

--- src/Entidades/Veiculo.java
+++ src/Entidades/Veiculo.java
@@ -1,6 +1,10 @@
 package Entidades;
 
-public class Veiculo {
+import java.io.Serializable;
+
+public class Veiculo implements Serializable {
+
+    private static final long serialVersionUID = -7566448629592086138L;
     private String tipo;
     private double vlDiaria;
     private String descricao;
